70c4242c3a0ad5d8075a438252058d03e27a89e3
pjenkys <Pjenkys@gmail.com>
2025-12-17 08:42:20 +0100

    Update sw.js
    Force update when something in index changed.

--- sw.js
+++ sw.js
@@ -1,24 +1,51 @@
-const CACHE_NAME = 'sklad-app-v1';
+// POKAŽDÉ, KDYŽ ZMĚNÍTE DATA NEBO HTML, ZVYŠTE TOTO ČÍSLO (v1 -> v2 -> v3...)
+const VERSION = 'v2'; 
+const CACHE_NAME = 'sklad-app-' + VERSION;
+
 const ASSETS = [
   './',
   './index.html',
   './data.csv',
   './manifest.json',
-  'https://unpkg.com/html5-qrcode',
-  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js'
+  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js'
 ];
 
-// Instalace - uložení do cache
+// 1. INSTALACE: Uloží nové soubory a okamžitě převezme kontrolu (skipWaiting)
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Důležité: Nečekat na zavření záložek, hned se instalovat
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Ukládám novou verzi:', VERSION);
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-// Získání dat - pokud jsme offline, ber z cache
+// 2. AKTIVACE: Smaže staré verze cache a převezme kontrolu nad stránkou
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          // Pokud klíč cache neodpovídá aktuální verzi, smaž ho
+          if (key !== CACHE_NAME) {
+            console.log('Mazání staré cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim(); // Okamžitě začít ovládat všechny otevřené stránky
+    })
+  );
+});
+
+// 3. FETCH: Pokud jsme offline, vrať cache. Pokud jsme online, zkus síť.
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
+      // Vrať z cache pokud existuje, jinak stahuj
       return response || fetch(e.request);
     })
   );
